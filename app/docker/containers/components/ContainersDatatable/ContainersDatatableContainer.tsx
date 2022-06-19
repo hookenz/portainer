149581c41e7943f9f1b711f99565db93b9b2c111
Chaim Lev-Ari <chiptus@gmail.com>
2022-06-19 15:27:37 +0300

    refactor(teams): migrate teams to react [EE-2273]
    closes [EE-2273]
    
    refactor(teams): move to react codebase

--- app/docker/containers/components/ContainersDatatable/ContainersDatatableContainer.tsx
+++ app/docker/containers/components/ContainersDatatable/ContainersDatatableContainer.tsx
@@ -31,7 +31,7 @@ export function ContainersDatatableContainer({
     <EnvironmentProvider environment={endpoint}>
       <TableSettingsProvider defaults={defaultSettings} storageKey={tableKey}>
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        <ContainersDatatable {...props} />
+        <ContainersDatatable {...props} tableKey={tableKey} />
       </TableSettingsProvider>
     </EnvironmentProvider>
   );
